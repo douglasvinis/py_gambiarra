6a641edde4ca1b588bc471c09e00b9fb313ffef7
Douglas Knowman <douglasknowman@gmail.com>
2016-04-12 13:02:05 -0300

    >added crop sprite image syste and show then on screen

--- pyanimview.py
+++ pyanimview.py
@@ -34,38 +34,64 @@ class AnimView:
         
         self.clock = None
         self.is_running = False
-        self.xmax = None
-        self.ymax = None
         self.xcount = 0
         self.ycount = 0
         self.count = 0
+        self.showsprite = None
 
         self.start_screen()
         self.loadSprite()
 
     def start_screen(self):
+        # starting the screen and clock
         self.screen = pygame.display.set_mode(WINDOW_SIZE)
         pygame.display.set_caption(WINDOW_NAME)
         self.clock = pygame.time.Clock()
 
     def loadSprite(self):
+        # load sprite image, calculate the size of the frames and make a surface to blit to.
         self.sprite = pygame.image.load(self.filepath)
-        print self.sprite.get_width()
-        self.xmax = self.sprite.get_width()/self.gridsize[0]
-        self.ymax = self.sprite.get_height()/self.gridsize[1]
-        
-        print self.spritecount
+        self.sprite_framesize = (self.sprite.get_width() / self.gridsize[0], self.sprite.get_height() / self.gridsize[1])
+        self.showsprite = pygame.Surface((self.sprite_framesize[0],self.sprite_framesize[1]))
     
     def start(self):
         print ">window open."
         self.is_running = True
-        self.clock.tick(self.framerate)
 
         while self.is_running :
+            self.clock.tick(self.framerate)
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     self.is_running = False
             self.screen.fill (BACKGROUND_COLOR)
+            # calculating position for crop the sprite image
+            x = self.xcount * self.sprite_framesize[0]
+            y = self.ycount * self.sprite_framesize[1]
+            width = (self.xcount+1) * self.sprite_framesize[0]
+            height = (self.ycount+1) * self.sprite_framesize[1]
+
+            self.xcount += 1
+            self.count += 1
+
+            # get each frame from sprite image and render on surface with the best size
+            self.showsprite.blit (self.sprite,(10,10),(x,y,width,height))
+
+            # centralize the sprite frames
+            centerx = (WINDOW_SIZE[0]/2) - self.sprite_framesize[0]/2
+            centery = (WINDOW_SIZE[1]/2) - self.sprite_framesize[1]/2
+            self.screen.blit (self.showsprite,(centerx,centery))
+
+            # if the viewer show every frame then stop.
+            if self.count == self.spritecount:
+                self.xcount = 0
+                self.ycount = 0
+
+            if self.xcount == self.gridsize[0]:
+                self.xcount = 0
+                self.ycount += 1
+                if self.ycount == self.gridsize[1]:
+                    self.ycount = 0
+
             pygame.display.flip()
             
         pygame.quit()
